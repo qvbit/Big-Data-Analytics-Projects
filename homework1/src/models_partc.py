18b4d11a8c4ca521a7daf9b7db2745863bdd50fe
Nabeel Nauman <nabeelnauman@Nabeels-MacBook-Pro.local>
2019-01-13 21:34:39 +0000

    Finished part 4.1d

--- homework1/src/models_partc.py
+++ homework1/src/models_partc.py
@@ -15,24 +15,32 @@ RANDOM_STATE = 545510477
 #input: X_train, Y_train and X_test
 #output: Y_pred
 def logistic_regression_pred(X_train, Y_train, X_test):
-	#TODO: train a logistic regression classifier using X_train and Y_train. Use this to predict labels of X_test
-	#use default params for the classifier	
-    
-	return None
+    #TODO: train a logistic regression classifier using X_train and Y_train. Use this to predict labels of X_test
+    #use default params for the classifier	
+    lr = LogisticRegression(random_state=RANDOM_STATE)
+    lr.fit(X_train, Y_train)
+    Y_pred = lr.predict(X_test)
+    return Y_pred
 
 #input: X_train, Y_train and X_test
 #output: Y_pred
 def svm_pred(X_train, Y_train, X_test):
 	#TODO:train a SVM classifier using X_train and Y_train. Use this to predict labels of X_test
 	#use default params for the classifier
-	return None
+	svm = LinearSVC(random_state=RANDOM_STATE)
+	svm.fit(X_train, Y_train)
+	Y_pred = svm.predict(X_test)
+	return Y_pred
 
 #input: X_train, Y_train and X_test
 #output: Y_pred
 def decisionTree_pred(X_train, Y_train, X_test):
 	#TODO:train a logistic regression classifier using X_train and Y_train. Use this to predict labels of X_test
 	#IMPORTANT: use max_depth as 5. Else your test cases might fail.
-	return None
+	dt = DecisionTreeClassifier(random_state=RANDOM_STATE, max_depth=5)
+	dt.fit(X_train, Y_train)
+	Y_pred = dt.predict(X_test)
+	return Y_pred
 
 
 #input: Y_pred,Y_true
@@ -40,7 +48,12 @@ def decisionTree_pred(X_train, Y_train, X_test):
 def classification_metrics(Y_pred, Y_true):
 	#TODO: Calculate the above mentioned metrics
 	#NOTE: It is important to provide the output in the same order
-	return None,None,None,None,None
+    accuracy = accuracy_score(Y_true, Y_pred)
+    auc = roc_auc_score(Y_true, Y_pred)
+    prec = precision_score(Y_true, Y_pred)
+    recall = recall_score(Y_true, Y_pred)
+    f1 = f1_score(Y_true, Y_pred)
+    return accuracy, auc, prec, recall, f1
 
 #input: Name of classifier, predicted labels, actual labels
 def display_metrics(classifierName,Y_pred,Y_true):
